6b695e5cf041f3c66a79e8c1468267af9237ce4d
panda <panda@developmentontheedge.com>
2017-09-03 21:30:55 +0700

    some bugs fixed in extension side

--- src/main/java/avac/AvacServlet.java
+++ src/main/java/avac/AvacServlet.java
@@ -22,7 +22,6 @@ public class AvacServlet extends HttpServlet
 {
     protected void doGet( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException
     {
-
         resp.setContentType( "text/html" );
         resp.setCharacterEncoding( UTF_8 );
         try
@@ -52,8 +51,7 @@ public class AvacServlet extends HttpServlet
 
     private void reqHandle( HttpServletRequest req, HttpServletResponse resp ) throws SQLException
     {
-
-        /* ---------------------
+        /** --------------------
          * Content.js parameters
          */
         String address = req.getParameter( GO_TO );
@@ -63,12 +61,12 @@ public class AvacServlet extends HttpServlet
 
         if( null != address && null != level && null != langFrom && null != langTo )
         {
-            /* -----------------------------
+            /** ----------------------------
              * Get all page paragraphs words
              */
             Set<String> wordSet = WebPage.getPageWords( address );
             String pageWords = AvacUtils.toInClause( wordSet );
-            /* ------------------------------------
+            /** -----------------------------------
              * Connect to avac schema for translate
              */
             AvacSchema avacSchema = new AvacSchema();
@@ -80,14 +78,13 @@ public class AvacServlet extends HttpServlet
 
             try
             {
-
                 String sql =
-                        "SELECT " +
+                        " SELECT " +
                         langFrom + "," +
                         langTo + " " +
-                        "FROM avac.avacDictionary av " +
-                        "WHERE av." + langFrom + " IN ( " + pageWords + " )" +
-                        "  AND av.rank > " + Math.pow( Double.parseDouble( level ), 2.547 );
+                        " FROM avac.avacDictionary av " +
+                        " WHERE av." + langFrom + " IN ( " + pageWords + " )" +
+                        "   AND av.rank > " + Math.pow( Double.parseDouble( level ), 2.547 );
                 System.out.println( "------------------------------------------------------------------------------------------\n" +
                                     "Выполняем запрос для: " +
                                     "-> " + address + "\n" +
@@ -96,8 +93,6 @@ public class AvacServlet extends HttpServlet
                                     "   langTo = " + langTo
                 );
                 rs = stmt.executeQuery( sql );
-
-                System.out.println( "------------------------------------------------------------------------------------------\n" + sql);
 
                 String langFromJSON;
                 String langToJSON;
@@ -125,7 +120,7 @@ public class AvacServlet extends HttpServlet
 
             System.out.println( "JSON size = " + json.size() );
 
-            /* ----------------------------------
+            /** ----------------------------------
              * Send json dictionary to content.js
              */
             try (PrintWriter writer = resp.getWriter())
